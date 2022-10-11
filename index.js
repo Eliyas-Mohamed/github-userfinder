8b71c4bc20414bd412948ed26aa0f2a0225f63c7
Mohamed Eliyas <61540226+Eliyas-Mohamed@users.noreply.github.com>
2022-10-11 13:11:10 +0530

    removed consoles

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ document.getElementById('username').addEventListener('keyup',startSearch);
 //starting search
 
 function startSearch(){
-    console.log(this.value)
+
     if(document.getElementById('username').value!==''){
-        console.log('havalue');
+        
         const searchValue=document.getElementById('username').value;
         github.getUser(searchValue)
         .then(res=>{
@@ -26,9 +26,7 @@ function startSearch(){
                 ui.undefinedAlert();
             }
             else{ 
-                console.log('uicalled');
-                console.log(res.response);
-                console.log(res.repos);
+                
                 ui.showProfile(res.response);
                 if(res.repos.length!==0){
                     ui.showRepos(res.repos);
